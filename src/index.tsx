d839f4a154fcc5584d4ff9dec0bbea143c6daae0
Arterixs <artemontugg@gmail.com>
2023-05-14 14:50:05 +0300

    chore: migration dependies in devDependies

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <HashRouter></HashRouter>
+  </React.StrictMode>
+);
