42007d4db65893275055b8da18264067d864db6d
Arterixs <artemontugg@gmail.com>
2023-05-19 16:05:11 +0300

    refactor: update params url auth

--- src/server/req-auth.ts
+++ src/server/req-auth.ts
@@ -1,21 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Authorization } from 'types/interface/server';
-import {
-  CLIENT_ID,
-  CLIENT_SECRET,
-  HR,
-  LOGIN_USER,
-  METHOD_AUTH,
-  PARAMS_AUTH,
-  PASSWORD,
-} from 'utils/constants/server-api';
+import { METHOD_AUTH, PARAMS_AUTH, PARAMS_AUTH_PASSWORD } from 'utils/constants/server-api';
 import { $apiBase } from './axios';
 
 export const Auth = async () => {
   try {
-    const response = await $apiBase.get<Authorization>(
-      `${METHOD_AUTH}${PARAMS_AUTH}?${LOGIN_USER}&${PASSWORD}&${CLIENT_ID}&${CLIENT_SECRET}&${HR}`
-    );
+    const response = await $apiBase.get<Authorization>(`${METHOD_AUTH}${PARAMS_AUTH}?${PARAMS_AUTH_PASSWORD}`);
     const result = response.data;
     console.log(result);
     return 1;
--- src/utils/constants/server-api.ts
+++ src/utils/constants/server-api.ts
@@ -1,10 +1,9 @@
+import { UserAuthData } from 'types/enums/server';
+
 export const VERSION_API = '2.0';
 export const BASE_URL = `https://startup-summer-2023-proxy.onrender.com/${VERSION_API}`;
 export const METHOD_AUTH = '/oauth2';
 export const PARAMS_AUTH = '/password';
-export const LOGIN_USER = 'login=sergei.stralenia@gmail.com';
-export const PASSWORD = 'password=paralect123';
-export const CLIENT_ID = 'client_id=2356';
-export const CLIENT_SECRET =
-  'client_secret=v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948';
-export const HR = 'hr=0';
+
+export const PARAMS_AUTH_PASSWORD = `?${UserAuthData.LOGIN}&${UserAuthData.PASSWORD}&${UserAuthData.ID}
+&${UserAuthData.SECRET}&${UserAuthData.HR}`;
