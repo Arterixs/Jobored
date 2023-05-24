a281067ea1df392f99b9dcbfc47d8df901bfbc69
Arterixs <artemontugg@gmail.com>
2023-05-24 05:50:52 +0300

    refactor: delete comments

--- src/utils/constants/server-api.ts
+++ src/utils/constants/server-api.ts
@@ -2,7 +2,6 @@ import { UserAuthData } from 'types/enums/server';
 
 export const VERSION_API = '2.0';
 export const BASE_URL = `https://startup-summer-2023-proxy.onrender.com/${VERSION_API}`;
-// export const BASE_URL = `https://startup-summer-proxy-production.up.railway.app/${VERSION_API}`;
 export const METHOD_AUTH = '/oauth2';
 export const PARAMS_AUTH = '/password';
 export const METHOD_JOB = '/catalogues';
